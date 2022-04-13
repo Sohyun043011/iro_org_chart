86027cbcc00066c33e49cd973a207f39be93f223
so1omon <solmin3665@gmail.com>
2022-04-13 11:09:57 +0900

    code refactoring

--- db_connect.js
+++ db_connect.js
@@ -28,6 +28,34 @@ module.exports={
     sleep : function(ms){
         const wakeUpTime = Date.now() + ms;
         while (Date.now() < wakeUpTime) {}
-    } 
-
+    },
+    emp_info_sync_query:`insert into good.emp_info 
+    SELECT NULL, A.emp_id, A.emp_nm as 'emp_name', IFNULL(A.mobile_no,'None') as mobile_no, 
+    IFNULL(A.office_tel_no,'None') as office_tel_no , A.org_nm as dept_name, 
+    IFNULL(A.mail_addr,'None') as mail_addr, IFNULL(A.roll_info,'None') as roll_info, 
+    IFNULL(C.post_nm, 'None') as post_name, IFNULL(C.duty_nm, 'None') as duty_name,
+    IFNULL(B.URL,'None') as img_url FROM connect.hr_info as A 
+    LEFT JOIN connect.gw_pic_info as B ON A.emp_id=B.emp_code 
+    RIGHT JOIN (SELECT distinct emp_id, post_nm, duty_nm from connect.inf_app 
+        WHERE (emp_id, sta_ymd, SEQ_NO) IN (
+            SELECT emp_id, sta_ymd, MAX(SEQ_NO) AS SEQ_NO FROM connect.inf_app 
+            WHERE (emp_id, sta_ymd) in(
+                SELECT emp_id, MAX(sta_ymd) AS sta_ymd FROM (
+                    SELECT emp_id, sta_ymd, SEQ_NO FROM connect.inf_app WHERE emp_id not in (
+                        SELECT emp_id FROM connect.inf_app WHERE appnt_nm IN('퇴직','파견계약해지')
+                    )
+                )w GROUP BY emp_id)
+            GROUP BY emp_id)
+        AND appnt_nm NOT IN ('직급대우해지') 
+        AND emp_nm NOT IN ('테과장','테스트')) AS C ON A.emp_id=C.emp_id`,
+    seat_info_sync_query_1:`INSERT INTO good.seat_info(emp_id, emp_name, dept_name, post_name, seat_arrng) 
+    SELECT emp_id, emp_name, dept_name, post_name, -1 FROM good.emp_info 
+    WHERE (emp_id, emp_name, dept_name, post_name) NOT IN (
+        SELECT emp_id, emp_name, dept_name, post_name FROM seat_info
+    )`,
+    seat_info_sync_query_2:`DELETE FROM good.seat_info 
+    WHERE (emp_id, emp_name, dept_name, post_name) NOT IN (
+        SELECT emp_id, emp_name, dept_name, post_name FROM good.emp_info
+    )`,
+    
 }
